bdafbde12c69043fcb888b656768daee7bbd9e84
Mia Anderson <meicy101@gmail.com>
2019-12-14 23:20:35 -0600

    final

--- main.py
+++ main.py
@@ -24,19 +24,20 @@ def display_time_form():
     email=request.form['email']
     user_name=request.form['username']
     home=''
+    usererror=''
     passworderror=''
     password_error=''
     email_error=''
     
     
-    while len(password)<4 or len(password)>20 :
+    if len(password)<4 or len(password)>20 :
         passworderror='Not a valid password'
 
         
         password=""
         vpassword=""
     
-    while password!=vpassword:
+    if  password!=vpassword:
         
 
         password_error = 'Passwords do not match'
@@ -44,13 +45,13 @@ def display_time_form():
         vpassword=""
         
     
-    while len(user_name)<4 or len(user_name)>20 :
+    if len(user_name)<4 or len(user_name)>20 :
         usererror='Not a valid username'
         password=""
         vpassword=""
         
          
-    while email>'':
+    if email>'':
         if '@' not in email or '.' not in email:
             email_error="That's not a valid email"
         
@@ -60,7 +61,7 @@ def display_time_form():
     if email_error=='' and usererror=='' and password_error=='' and passworderror=='':
         return render_template('welcome.html', home=user_name)
     else:
-        return render_template('main_form.html',passworderror=passworderror,password_error=password_error,password=password,vpassword=vpassword, home=user_name,book=email,email_error=email_error)
+        return render_template('main_form.html',passworderror=passworderror,password_error=password_error,password=password,vpassword=vpassword, home=user_name,book=email,email_error=email_error, usererror=usererror)
 
 
 
